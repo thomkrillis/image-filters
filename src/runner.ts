6076f653aefba2dd8f658943d3706a51b3aafd23
thomkrillis <thisisjackolantern@gmail.com>
2019-07-02 23:53:37 -0400

    Add more devtools, add webpack, attempt some silly things with static asset loading and fs

--- src/runner.ts
+++ src/runner.ts
@@ -1,7 +1,11 @@
+import { readFile } from "fs";
+// import { promisify } from "util";
 import {
   cycle,
   helloWorld,
 } from "./";
+// import testImage from "./assets/sample.jpg";
+const testImage = require("./assets/sample.jpg");
 
 helloWorld();
 
@@ -12,3 +16,14 @@ const img = {
 };
 
 cycle(img);
+
+// const read = promisify(fs.readFile);
+// read(__dirname + "/assets/sample.jpg").then((data) => {
+//   console.log("d", data);
+// });
+
+readFile(__dirname + "/assets/sample.jpg", (err, data) => {
+  console.log("d", data);
+});
+
+console.log("h", testImage);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/runner.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  node: {
+    fs: "empty"
+  }
+};
