083d7703e8a23cfcab434efa6fd1c0b45cea1a6a
thomkrillis <thisisjackolantern@gmail.com>
2019-07-08 21:45:38 -0400

    Refactor runner, add writing output file for testing

--- src/runner.ts
+++ src/runner.ts
@@ -1,20 +1,31 @@
-import { readFile } from "fs";
+import { readFile, writeFile } from "fs";
 import { promisify } from "util";
 import {
   cycle,
   helloWorld,
 } from "./";
 
-helloWorld();
+const run = async () => {
+  helloWorld();
 
-const img = {
-  b: [],
-  g: [],
-  r: [],
+  const img = {
+    b: [],
+    g: [],
+    r: [],
+  };
+
+  cycle(img);
+
+  const data = await promisify(readFile)(__dirname + "/assets/sample.jpg");
+  console.log("d1", data);
+  await promisify(writeFile)(__dirname + "/assets/output.jpg", data);
+  return null;
 };
 
-cycle(img);
-
-promisify(readFile)(__dirname + "/assets/sample.jpg").then((data) => {
-  console.log("d1", data);
+run().then(() => {
+  console.log("Success.");
+  process.exit(0);
+}).catch((err) => {
+  console.log(`Error: ${err}`);
+  process.exit(1);
 });
